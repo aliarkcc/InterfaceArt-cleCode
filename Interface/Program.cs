8c488439afe3ece54378cfc8409c62a1638b3066
Mehmet Ali Arkac <markac@developtica.com>
2022-10-17 00:09:51 +0300

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new CorporateCustomerManager());
+            customerManager.TakeAction();
+            Console.ReadLine();
+        }
+
+        public interface ICustomerManager
+        {
+            void Add();
+        }
+
+        public class CustomerManager
+        {
+            private ICustomerManager _customerManager;
+
+            public CustomerManager(ICustomerManager customerManager)
+            {
+                _customerManager = customerManager;
+            }
+
+            public void TakeAction()
+            {
+                _customerManager.Add();
+            }
+        }
+
+        public class IndividualCustomerManager : ICustomerManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Corporate Customer Add");
+            }
+        }
+        public class CorporateCustomerManager : ICustomerManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Individual Customer Add");
+            }
+        }
+    }
+}
